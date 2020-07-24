dec1894c2229144310920ae3b6c26e8ccd0492ed
dancan-sandys <dancan.oruko96@gmail.com>
2020-07-24 15:07:27 +0300

    link jquery library and main.js then add a function to toggle between the 'what we do' icons and their respective descriptions

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(`.design`).click(function(){
+    $(`.design img`).toggle();
+    $(`#design`).toggle();
+})
+
+$(`.development`).click(function(){
+    $(`.development img`).toggle();
+    $(`#development`).toggle();
+})
+
+$(`.product`).click(function(){
+    $(`.product img`).toggle();
+    $(`#product`).toggle();
+})
