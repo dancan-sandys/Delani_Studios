a0443c8ed1b4d1f6e1d48bcdc295171cbdd29dbd
dancan-sandys <dancan.oruko96@gmail.com>
2020-07-25 14:32:35 +0300

    refactore the icons to be toggle to occupy less space on jquery

--- js/main.js
+++ js/main.js
@@ -1,132 +1,56 @@
-var whitesquares =[`whitesquare1`, `whitesquare2`, `whitesquare3`, `whitesquare4`, `whitesquare5`, `whitesquare6`, `whitesquare7`, `whitesquare8`,]
+var whitesquares = [`whitesquare1`, `whitesquare2`, `whitesquare3`, `whitesquare4`, `whitesquare5`, `whitesquare6`, `whitesquare7`, `whitesquare8`,]
+var whatwedoes =[`design`, `development`, `product`]
 
-
-function formsubmit(){
+function formsubmit() {
     var name = document.getElementById(`name`).value;
     var email = document.getElementById(`email`).value;
     var feedback = document.getElementById(`feedback`).value;
 
-    
-    
-    
-    if(name=== ``){
+
+
+
+    if (name === ``) {
         alert(`*Name field is required`)
     }
 
-    if(email===``){
+    if (email === ``) {
         alert(`*Your email address is required`)
     }
 
-    if(feedback===``){
+    if (feedback === ``) {
         alert(`*please enter something`)
     }
-    
-   if(name !=`` && email !=`` && feedback != ``){
-    
-    
-       var filledname = name;
-       var filledemail = email;
-       var filledfeedback = feedback;
 
-    
-    
-       alert(`Thank you for your feedback, we will be intouch soon.`)
-   }
+    if (name != `` && email != `` && feedback != ``) {
+
+
+        var filledname = name;
+        var filledemail = email;
+        var filledfeedback = feedback;
+
+
+
+        alert(`Thank you for your feedback, we will be intouch soon.`)
+    }
 
 }
 
 
-
-$(`.design`).click(function(){
-    $(`.design img`).slideToggle();
-    $(`#design`).slideToggle();
+whatwedoes.forEach(function(whatwedo){
+$(`.${whatwedo}`).click(function () {
+    $(`.${whatwedo} img`).slideToggle();
+    $(`#${whatwedo}`).slideToggle();
 })
-
-$(`.development`).click(function(){
-    $(`.development img`).slideToggle();
-    $(`#development`).slideToggle();
-})
-
-$(`.product`).click(function(){
-    $(`.product img`).slideToggle();
-    $(`#product`).slideToggle();
 })
 
 
-whitesquares.forEach(function(whitesquare){
+whitesquares.forEach(function (whitesquare) {
 
+    $(`#${whitesquare}`).mouseover(function () {
+        $(`#${whitesquare}`).css(`opacity`, `0.7`);
+    })
 
-$(`#${whitesquare}`).mouseover(function(){
-    $(`#${whitesquare}`).css(`opacity`, `0.7`);
+    $(`#${whitesquare}`).mouseout(function () {
+        $(`#${whitesquare}`).css(`opacity`, `0`);
+    })
 })
-
-
-
- $(`#${whitesquare}`).mouseout(function(){
-     $(`#${whitesquare}`).css(`opacity`, `0`);
- })
-
-})
-// $(`#whitesquare2`).mouseover(function(){
-//     $(`#whitesquare2`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare2`).mouseout(function(){
-//     $(`#whitesquare2`).css(`opacity`, `0`);
-// })
-
-
-// $(`#whitesquare3`).mouseover(function(){
-//     $(`#whitesquare3`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare3`).mouseout(function(){
-//     $(`#whitesquare3`).css(`opacity`, `0`);
-// })
-
-
-// $(`#whitesquare4`).mouseover(function(){
-//     $(`#whitesquare4`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare4`).mouseout(function(){
-//     $(`#whitesquare4`).css(`opacity`, `0`);
-// })
-
-
-// $(`#whitesquare5`).mouseover(function(){
-//     $(`#whitesquare5`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare5`).mouseout(function(){
-//     $(`#whitesquare5`).css(`opacity`, `0`);
-// })
-
-
-
-// $(`#whitesquare6`).mouseover(function(){
-//     $(`#whitesquare6`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare6`).mouseout(function(){
-//     $(`#whitesquare6`).css(`opacity`, `0`);
-// })
-
-
-// $(`#whitesquare7`).mouseover(function(){
-//     $(`#whitesquare7`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare7`).mouseout(function(){
-//     $(`#whitesquare7`).css(`opacity`, `0`);
-// })
-
-
-// $(`#whitesquare8`).mouseover(function(){
-//     $(`#whitesquare8`).css(`opacity`, `0.7`);
-// })
-
-// $(`#whitesquare8`).mouseout(function(){
-//     $(`#whitesquare8`).css(`opacity`, `0`);
-// })
-
