ea01148704e794f9743bb67338981f7f683b10af
dancan-sandys <dancan.oruko96@gmail.com>
2020-07-25 14:25:47 +0300

    refactor the hover effects on jquery to make it more presentable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var whitesquares =[`whitesquare1`, `whitesquare2`, `whitesquare3`, `whitesquare4`, `whitesquare5`, `whitesquare6`, `whitesquare7`, `whitesquare8`,]
+
+
 function formsubmit(){
     var name = document.getElementById(`name`).value;
     var email = document.getElementById(`email`).value;
@@ -49,75 +52,81 @@ $(`.product`).click(function(){
     $(`#product`).slideToggle();
 })
 
-$(`#whitesquare1`).mouseover(function(){
-    $(`#whitesquare1`).css(`opacity`, `0.7`);
-})
 
-$(`#whitesquare1`).mouseout(function(){
-    $(`#whitesquare1`).css(`opacity`, `0`);
-})
+whitesquares.forEach(function(whitesquare){
 
 
-$(`#whitesquare2`).mouseover(function(){
-    $(`#whitesquare2`).css(`opacity`, `0.7`);
-})
-
-$(`#whitesquare2`).mouseout(function(){
-    $(`#whitesquare2`).css(`opacity`, `0`);
-})
-
-
-$(`#whitesquare3`).mouseover(function(){
-    $(`#whitesquare3`).css(`opacity`, `0.7`);
-})
-
-$(`#whitesquare3`).mouseout(function(){
-    $(`#whitesquare3`).css(`opacity`, `0`);
-})
-
-
-$(`#whitesquare4`).mouseover(function(){
-    $(`#whitesquare4`).css(`opacity`, `0.7`);
-})
-
-$(`#whitesquare4`).mouseout(function(){
-    $(`#whitesquare4`).css(`opacity`, `0`);
-})
-
-
-$(`#whitesquare5`).mouseover(function(){
-    $(`#whitesquare5`).css(`opacity`, `0.7`);
-})
-
-$(`#whitesquare5`).mouseout(function(){
-    $(`#whitesquare5`).css(`opacity`, `0`);
+$(`#${whitesquare}`).mouseover(function(){
+    $(`#${whitesquare}`).css(`opacity`, `0.7`);
 })
 
 
 
-$(`#whitesquare6`).mouseover(function(){
-    $(`#whitesquare6`).css(`opacity`, `0.7`);
+ $(`#${whitesquare}`).mouseout(function(){
+     $(`#${whitesquare}`).css(`opacity`, `0`);
+ })
+
 })
+// $(`#whitesquare2`).mouseover(function(){
+//     $(`#whitesquare2`).css(`opacity`, `0.7`);
+// })
 
-$(`#whitesquare6`).mouseout(function(){
-    $(`#whitesquare6`).css(`opacity`, `0`);
-})
-
-
-$(`#whitesquare7`).mouseover(function(){
-    $(`#whitesquare7`).css(`opacity`, `0.7`);
-})
-
-$(`#whitesquare7`).mouseout(function(){
-    $(`#whitesquare7`).css(`opacity`, `0`);
-})
+// $(`#whitesquare2`).mouseout(function(){
+//     $(`#whitesquare2`).css(`opacity`, `0`);
+// })
 
 
-$(`#whitesquare8`).mouseover(function(){
-    $(`#whitesquare8`).css(`opacity`, `0.7`);
-})
+// $(`#whitesquare3`).mouseover(function(){
+//     $(`#whitesquare3`).css(`opacity`, `0.7`);
+// })
 
-$(`#whitesquare8`).mouseout(function(){
-    $(`#whitesquare8`).css(`opacity`, `0`);
-})
+// $(`#whitesquare3`).mouseout(function(){
+//     $(`#whitesquare3`).css(`opacity`, `0`);
+// })
+
+
+// $(`#whitesquare4`).mouseover(function(){
+//     $(`#whitesquare4`).css(`opacity`, `0.7`);
+// })
+
+// $(`#whitesquare4`).mouseout(function(){
+//     $(`#whitesquare4`).css(`opacity`, `0`);
+// })
+
+
+// $(`#whitesquare5`).mouseover(function(){
+//     $(`#whitesquare5`).css(`opacity`, `0.7`);
+// })
+
+// $(`#whitesquare5`).mouseout(function(){
+//     $(`#whitesquare5`).css(`opacity`, `0`);
+// })
+
+
+
+// $(`#whitesquare6`).mouseover(function(){
+//     $(`#whitesquare6`).css(`opacity`, `0.7`);
+// })
+
+// $(`#whitesquare6`).mouseout(function(){
+//     $(`#whitesquare6`).css(`opacity`, `0`);
+// })
+
+
+// $(`#whitesquare7`).mouseover(function(){
+//     $(`#whitesquare7`).css(`opacity`, `0.7`);
+// })
+
+// $(`#whitesquare7`).mouseout(function(){
+//     $(`#whitesquare7`).css(`opacity`, `0`);
+// })
+
+
+// $(`#whitesquare8`).mouseover(function(){
+//     $(`#whitesquare8`).css(`opacity`, `0.7`);
+// })
+
+// $(`#whitesquare8`).mouseout(function(){
+//     $(`#whitesquare8`).css(`opacity`, `0`);
+// })
 
