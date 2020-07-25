637a156f9a26487353b714665dd663aeb649d97b
dancan-sandys <dancan.oruko96@gmail.com>
2020-07-25 12:53:05 +0300

    add a function to obtain information from the form and alert user that we have received it and also alert incasse one of the fields is not filled

--- js/main.js
+++ js/main.js
@@ -13,4 +13,29 @@ $(`.product`).click(function(){
     $(`#product`).toggle();
 })
 
+function formsubmit(){
+    var name = document.getElementById(`name`).value;
+    var email = document.getElementById(`email`).value;
+    var feedback = document.getElementById(`feedback`).value;
 
+    if(name=== ``){
+        alert(`*Name field is required`)
+    }
+
+    if(email===``){
+        alert(`*Your email address is required`)
+    }
+
+    if(feedback===``){
+        alert(`*please enter something`)
+    }
+    
+   if(name !=`` && email !=`` && feedback != ``){
+       var filledname = name;
+       var filledemail = email;
+       var filledfeedback = feedback;
+
+       alert(`Thank you for your feedback, we will be intouch soon.`)
+   }
+
+}
